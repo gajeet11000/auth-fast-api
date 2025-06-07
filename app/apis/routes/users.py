9ed756ec195f415db1eb3677dd15f408c5865223
G Ajeet <gajeet11000@gmail.com>
2025-06-08 00:52:01 +0530

    handle uniquie integrity error

--- app/apis/routes/users.py
+++ app/apis/routes/users.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
+from sqlalchemy.exc import IntegrityError
 
+from app import serializers
 from app.db import models
 from app.db.session import get_db_session
-from app import serializers
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -10,8 +11,18 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/", summary="Create User")
 def create_user(request: Request, user: serializers.CreateUser):
     with get_db_session() as db:
-        db_user = models.User(**user.model_dump())
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
+        try:
+            db_user = models.User(**user.model_dump())
+            db.add(db_user)
+            db.commit()
+            db.refresh(db_user)
+            return db_user
+        except IntegrityError as e:
+            db.rollback()
+            if "ix_users_email" in str(e.orig):
+                raise HTTPException(
+                    status_code=409, detail="A user with this email already exists."
+                )
+            raise HTTPException(
+                status_code=500, detail="An unexpected database error occurred."
+            )
